54a503492985036a4d0bc34b359042484fd6c992
potykion <potykion@gmail.com>
2020-01-05 02:25:26 +0300

    PushoverNotificationSender

--- api.py
+++ api.py
@@ -3,14 +3,33 @@ import os
 from dotenv import load_dotenv
 from notifiers import notify, Response
 
-# грузим енвы
-load_dotenv()
-PUSHOVER_USER = os.environ["PUSHOVER_USER"]
-PUSHOVER_TOKEN = os.environ["PUSHOVER_TOKEN"]
 
-# тестим отправку пушей (https://pushover.net/, https://github.com/notifiers/notifiers)
-message = 'test'
-res: Response = notify('pushover', user=PUSHOVER_USER, token=PUSHOVER_TOKEN, message=message)
-assert res.ok
+class PushoverNotificationSender:
+    """
+    Класс для отправки уведомлений с помощью Pushover
+    https://pushover.net/
+    https://github.com/notifiers/notifiers
+    """
+
+    def __init__(self, user: str, token: str) -> None:
+        self.service = 'pushover'
+        self.user = user
+        self.token = token
+
+    def send(self, message: str) -> bool:
+        res: Response = notify(self.service, user=self.user, token=self.token, message=message)
+        return res.ok
 
 
+def main() -> None:
+    load_dotenv()
+    PUSHOVER_USER = os.environ["PUSHOVER_USER"]
+    PUSHOVER_TOKEN = os.environ["PUSHOVER_TOKEN"]
+
+    sender = PushoverNotificationSender(PUSHOVER_USER, PUSHOVER_TOKEN)
+    message = 'test'
+    sender.send(message)
+
+
+if __name__ == '__main__':
+    main()
